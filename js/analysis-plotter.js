4f9df37ae3442f94970e4565c5aa4640bf0211f4
Fakhrizal Mustaqim <rizalmustaqim150@gmail.com>
2024-12-25 06:48:15 +0700

    assignment

--- js/analysis-plotter.js
+++ js/analysis-plotter.js
@@ -4,8 +4,9 @@
  * Plot result from the beam analysis calculation into a graph
  */
 class AnalysisPlotter {
-    constructor(container) {
-        this.container = container;
+    constructor(canvasId) {
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext('2d');
     }
 
     /**
@@ -13,7 +14,28 @@ class AnalysisPlotter {
      *
      * @param {Object{beam : Beam, load : float, equation: Function}}  The equation data
      */
-    plot(data) {
-        console.log('Plotting data : ', data);
+    plot(data, options) {
+        const ctx = this.canvas.getContext('2d');
+        new Chart(ctx, {
+            type: options.type || 'line',
+            data: {
+                labels: data.x,
+                datasets: [{
+                    label: options.label || 'Analysis Result',
+                    data: data.y,
+                    borderColor: options.color || 'blue',
+                    borderWidth: 2,
+                    fill: false
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    x: { title: { display: true, text: options.xLabel || 'Position (x)' } },
+                    y: { title: { display: true, text: options.yLabel || 'Value (y)' } }
+                }
+            }
+        });
+        // console.log('Plotting data : ', data);
     }
 }
